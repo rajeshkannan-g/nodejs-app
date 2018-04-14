a7ddc0bbedaf19608e4d40d49a7881c5222cee4d
Rajeshkannan Gurusamy <rajesh.kannan@servicenow.com>
2018-04-14 21:46:08 +0530

    Debug app start issue

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ else
   initApp();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.listen(port, ip, () => console.log(`Server running on http://${ip}:${port}`));
 
