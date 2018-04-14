f23e204dbcc860f4a7138d733a5b1d5dfbe076ff
Rajeshkannan Gurusamy <rajesh.kannan@servicenow.com>
2018-04-14 22:31:47 +0530

    remove log statement

--- app/db.js
+++ app/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const dbConfig = require('./config/db_config');
 if(dbConfig.dbURL != null) {
-    console.log(dbConfig.dbURL);
     mongoose.connect(dbConfig.dbURL, dbConfig.dbOptions, function (err, conn) {
         if (err) {
             console.error(err);
