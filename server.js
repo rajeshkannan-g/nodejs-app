cfbce12d0f043e5a614396b2115b3541830a227a
Rajeshkannan Gurusamy <rajesh.kannan@servicenow.com>
2018-04-14 21:53:20 +0530

    add test page

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ else
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+  
+
+app.get('/hi', function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end('Hello there!');
+});
 
 app.listen(port, ip, () => console.log(`Server running on http://${ip}:${port}`));
 
